e9396bfbb140714b50022d71ab43883d97d39329
Matt Hand <mmhand123@gmail.com>
2018-12-09 11:44:35 -0500

    Finish part 2 of day 1

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,15 +1,61 @@
 import { data } from './input.json';
 
-function parseData(arr: ReadonlyArray<string>): number {
+function partOne(arr: ReadonlyArray<string>): number {
   return arr.reduce((acc, item) => {
     console.log(acc, item);
 
-    const operator = item[0];
-    const value = parseInt(item.substr(1), 10);
+    const { operator, value } = parseItem(item);
 
-    const newValue = operator === '+' ? acc + value : acc - value;
+    const newValue = getFrequency(operator, acc, value);
     return newValue;
   }, 0);
 }
 
-console.log(parseData(data));
+function partTwo(arr: string[]): number {
+  let index = 0;
+  const end = arr.length - 1;
+  let firstDuplicateFrequency: number;
+  let currentFrequency: number = 0;
+  const frequencyMap = new Map<number, boolean>();
+
+  while (firstDuplicateFrequency === undefined) {
+    if (index > end) {
+      index = 0;
+    }
+
+    if (frequencyMap.has(currentFrequency)) {
+      firstDuplicateFrequency = currentFrequency;
+    } else {
+      frequencyMap.set(currentFrequency, true);
+
+      const { operator, value } = parseItem(arr[index]);
+      const newFrequency = getFrequency(operator, currentFrequency, value);
+
+      currentFrequency = newFrequency;
+
+      index++;
+    }
+  }
+  return firstDuplicateFrequency;
+}
+
+function getFrequency(
+  operator: '+' | '-',
+  oldFrequency: number,
+  value: number
+) {
+  return operator === '+' ? oldFrequency + value : oldFrequency - value;
+}
+
+function parseItem(item: string): { operator: '+' | '-'; value: number } {
+  const operator = item[0] as '+' | '-';
+  const value = parseInt(item.substr(1), 10);
+
+  return {
+    operator,
+    value
+  };
+}
+
+// console.log(partOne(data));
+console.log(partTwo(data));
