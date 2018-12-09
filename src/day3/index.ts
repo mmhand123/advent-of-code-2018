979fba2e20b3824c3b93886751592e3ec57f3afd
Matt Hand <mmhand123@gmail.com>
2018-12-09 13:59:17 -0500

    Part two of day three

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -2,6 +2,7 @@ import { values } from './input.json';
 
 interface FabrircClaim {
   id: string;
+  hasOverlaps: boolean;
   rectangle: Rectangle;
 }
 
@@ -37,6 +38,7 @@ function parseClaimString(claimString: string): FabrircClaim {
   const [width, height] = dimensions.split('x');
 
   return {
+    hasOverlaps: false,
     id,
     rectangle: {
       height: parseInt(height, 10),
@@ -77,6 +79,56 @@ function partOne(claims: string[]): number {
   }, 0);
 }
 
+interface EnhancedClaimBoard {
+  [key: string]: { firstClaim: string; overlapped: boolean };
+}
+
+function partTwo(claims: string[]): any {
+  const parsedClaimsMap: { [key: string]: FabrircClaim } = claims
+    .map(claim => parseClaimString(claim))
+    .reduce((claimMap, claim) => {
+      claimMap[claim.id] = claim;
+      return claimMap;
+    }, {});
+  const claimBoard: EnhancedClaimBoard = {};
+
+  Object.keys(parsedClaimsMap).forEach(claimId => {
+    const claim = parsedClaimsMap[claimId];
+
+    const { x: startX, y: startY, width, height } = claim.rectangle;
+
+    for (let x = startX; x < startX + width; x++) {
+      for (let y = startY; y < startY + height; y++) {
+        const square = `${x},${y}`;
+
+        if (!claimBoard.hasOwnProperty(square)) {
+          // first time, just claim
+          claimBoard[square] = {
+            firstClaim: claim.id,
+            overlapped: false
+          };
+        } else if (
+          claimBoard.hasOwnProperty(square) &&
+          !claimBoard[square].overlapped
+        ) {
+          // we have an overlap for the first time
+          claimBoard[square].overlapped = true;
+          parsedClaimsMap[claim.id].hasOverlaps = true;
+          parsedClaimsMap[claimBoard[square].firstClaim].hasOverlaps = true;
+        } else {
+          parsedClaimsMap[claim.id].hasOverlaps = true;
+          // we have an overlap for the second time
+        }
+      }
+    }
+  });
+
+  return Object.keys(parsedClaimsMap).filter(claimId => {
+    const claim = parsedClaimsMap[claimId];
+    return !claim.hasOverlaps;
+  });
+}
+
 // const fakeClaims = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2'];
 
-console.log(partOne(values));
+console.log(partTwo(values));
